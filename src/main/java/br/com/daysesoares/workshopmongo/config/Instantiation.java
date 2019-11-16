011574a6e128247390087139c1c6df53569f44d0
Dayse Soares <daysedesign@gmail.com>
2019-11-16 14:05:38 -0300

    Acrescentando comentários aos posts

--- src/main/java/br/com/daysesoares/workshopmongo/config/Instantiation.java
+++ src/main/java/br/com/daysesoares/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import br.com.daysesoares.workshopmongo.domain.Post;
 import br.com.daysesoares.workshopmongo.domain.User;
 import br.com.daysesoares.workshopmongo.dto.AuthorDTO;
+import br.com.daysesoares.workshopmongo.dto.CommentDTO;
 import br.com.daysesoares.workshopmongo.repository.PostRepository;
 import br.com.daysesoares.workshopmongo.repository.UserRepository;
 
@@ -45,6 +46,16 @@ public class Instantiation implements CommandLineRunner{
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("16/11/2019"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("16/11/2019"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo bom dia!", sdf.parse("16/11/2019"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
+		
 		
 	}
 
