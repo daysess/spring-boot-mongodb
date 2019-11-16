0abd6040124fbe7dd10164fadee9774edc7e2e82
Dayse Soares <daysedesign@gmail.com>
2019-11-16 13:46:51 -0300

    Obtendo um post por id

---
+++ src/main/java/br/com/daysesoares/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package br.com.daysesoares.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.daysesoares.workshopmongo.domain.Post;
+import br.com.daysesoares.workshopmongo.repository.PostRepository;
+import br.com.daysesoares.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		if(!post.isPresent()) {
+			throw new ObjectNotFoundException("Objeto não encontrado.");
+		}
+		
+		return post.get();
+	}
+	
+}
